229d9ab5066b124cfa7f10a1fdb03b02fc54bcb8
Camille TJHOA <camille@contract-live.com>
2017-05-15 22:21:59 +0200

    Update nom + add more parsers

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,95 @@
 #[macro_use]
 extern crate nom;
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
 
 use nom::{IResult, space, alphanumeric, eol};
 
 use std::str;
 
-// Entry:
-// - Entity
-// - Macro
-// - Comment
+// TODO: body ::= (_* NL)* (entry NL)* entry? EOF
+// TODO: entry ::= comment | section | message
+
+named!(comment<&str>, map_res!(do_parse!(
+    char!('/') >>
+    char!('/') >>
+    value: take_while!(call!(|c| {
+        c != '\r' as u8 &&
+        c != '\n' as u8
+    })) >>
+    eol >>
+    (value)
+), str::from_utf8));
+named!(section<&str>, map_res!(delimited!(
+    tag!("[["),
+    variant_symbol,
+    tag!("]]")
+), str::from_utf8));
+
+named!(break_indent<&[u8], ()>, do_parse!(
+    many1!(
+        do_parse!(
+            many0!(eol) >>
+            space >>
+            ()
+        )
+    ) >>
+    ()
+));
+
+named!(identifier, re_bytes_find_static!(r"^[a-zA-Z_?-][a-zA-Z0-9_?-]*"));
+named!(external, do_parse!(
+    char!('$') >>
+    identifier: identifier >>
+    (identifier)
+));
 
 named!(word, take_while!(call!(|c| {
     c != '{' as u8 &&
-        c != '}' as u8 &&
-        c != '[' as u8 &&
-        c != ']' as u8 &&
-        c != '\n' as u8 &&
-        c != '\\' as u8
+    c != '}' as u8 &&
+    c != '[' as u8 &&
+    c != ']' as u8 &&
+    c != '\n' as u8 &&
+    c != '\r' as u8 &&
+    c != '\\' as u8
 })));
+named!(builtin, re_bytes_find_static!(r"^[A-Z_?-]+"));
+named!(number, re_bytes_find_static!(r"^[-+]?[0-9]*\.?[0-9]+"));
 
-named!(section<&str>, map_res!(
-    delimited!(
-        tag!("[["),
-        word,
-        tag!("]]")
-    ),
-    str::from_utf8
+named!(variant_key, alt!(number | variant_symbol));
+named!(variant_symbol, take_while!(call!(|c| {
+    c != '{' as u8 &&
+    c != '}' as u8 &&
+    c != '[' as u8 &&
+    c != ']' as u8 &&
+    c != '\n' as u8 &&
+    c != '\r' as u8 &&
+    c != ' ' as u8 &&
+    c != '\t' as u8 &&
+    c != '\\' as u8
+})));
+// TODO: variant ::= NL __ '[' _? variant-key _? ']' __ pattern
+// TODO: default-variant ::= NL __ '*[' _? variant-key _? ']' __ pattern
+// TODO: variant-list ::= variant* default-variant variant*
+
+named!(tag, do_parse!(
+    char!('#') >>
+    word: word >>
+    (word)
 ));
+named!(tag_list <Vec<&str> >, do_parse!(
+    eol >>
+    list: many1!(do_parse!(
+        break_indent >>
+        tag: map_res!(tag, str::from_utf8) >>
+        (tag)
+    )) >>
+    (list)
+));
+
+// TODO: attribute ::= NL __ '.' identifier value
+// TODO: attribute-list ::= attribute+
 
 named!(value <Vec<&str> >,
     alt!(
@@ -69,15 +132,47 @@ named!(entity_value <&[u8], (&str,Vec<&str>)>,
 );
 
 #[test]
+fn parse_identifier_test() {
+    let ftl = &b"iden-tifi?er blah"[..];
+
+    let ftl_without_identifier = &b" blah"[..];
+
+    let res = identifier(ftl);
+    println!("{:?}", res);
+    match res {
+        IResult::Done(i, o) => println!("i: {:?} | o: {:?}", str::from_utf8(i), o),
+        _ => println!("error")
+    }
+
+    assert_eq!(res, IResult::Done(ftl_without_identifier, &b"iden-tifi?er"[..]));
+}
+
+#[test]
+fn parse_external_test() {
+    let ftl = &b"$iden-tifi?er blah"[..];
+
+    let ftl_without_external = &b" blah"[..];
+
+    let res = external(ftl);
+    println!("{:?}", res);
+    match res {
+        IResult::Done(i, o) => println!("i: {:?} | o: {:?}", str::from_utf8(i), o),
+        _ => println!("error")
+    }
+
+    assert_eq!(res, IResult::Done(ftl_without_external, &b"iden-tifi?er"[..]));
+}
+
+#[test]
 fn parse_section_test() {
-    let ftl_file = &b"[[section]]
+    let ftl = &b"[[section]]
 entity1=value1
 entity2 = value2"[..];
 
     let ftl_without_section = &b"\nentity1=value1
 entity2 = value2"[..];
 
-    let res = section(ftl_file);
+    let res = section(ftl);
     println!("{:?}", res);
     match res {
         IResult::Done(i, o) => println!("i: {:?} | o: {:?}", str::from_utf8(i), o),
